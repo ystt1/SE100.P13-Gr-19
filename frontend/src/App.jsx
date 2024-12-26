456b5e5510a4d5c6d3aa2f9c066169dd34b0254d
huynhtrung1904 <huynhtrung1904@gmail.com>
2024-12-26 22:25:06 +0700

    check condition blank

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,9 +55,9 @@ function App() {
           <Route
             path="/quiz"
             element={
-              <ProtectedRoute>
+             <ProtectedRoute>
                 <Quiz />
-              </ProtectedRoute>
+             </ProtectedRoute>
             }
           />
           <Route
