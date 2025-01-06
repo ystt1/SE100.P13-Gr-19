59acbec839eeb45e192078190e7192f7ea09f794
unknown <21521637@gm.uit.edu.vn>
2025-01-06 23:48:17 +0700

    add some team service

--- frontend/src/data/service/team_service.ts
+++ frontend/src/data/service/team_service.ts
@@ -1,17 +1,18 @@
 import axiosInstance from "../../core/instant_service/instant_service";
 import axios from 'axios';
 
-interface TopicAddPayload {
+interface TeamAddPayLoad {
   name: string;
-  description: string;
+  maxParticipant: number;
 }
 
 
 
 const TeamService = {
-  addTeams: async (topicAddPayload: TopicAddPayload) => {
+  //add team truyền vào name,maxParticipant
+  addTeams: async (topicAddPayload: TeamAddPayLoad) => {
     try {
-      const response = await axiosInstance.post("/teams/create", topicAddPayload);
+      const response = await axiosInstance.post("/team", topicAddPayload);
       console.log(response);
       
       if (response.status == 200) {
@@ -31,7 +32,74 @@ const TeamService = {
 
       return "Add Team Fail";
     }
-  }
+  },
+  getAllTeam: async (page: number, limit: number, search = "", sortElement = "", direction = "") => {
+    try {
+      const params: any = { page, limit };
+      if (search) params.search = search;
+      if (sortElement) params.sortElement = sortElement;
+      if (direction) params.direction = direction;
+
+      const response = await axiosInstance.get("/team/all", { params });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching topics:", error);
+      throw error;
+    }
+  },
+  getMyTeams: async (page: number, limit: number, search = "", sortElement = "", direction = "") => {
+    try {
+      const params: any = { page, limit };
+      if (search) params.search = search;
+      if (sortElement) params.sortElement = sortElement;
+      if (direction) params.direction = direction;
+
+      const response = await axiosInstance.get("/team/my-teams", { params });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching topics:", error);
+      throw error;
+    }
+  },
+  getJoinedTeams: async (page: number, limit: number, search = "", sortElement = "", direction = "") => {
+    try {
+      const params: any = { page, limit };
+      if (search) params.search = search;
+      if (sortElement) params.sortElement = sortElement;
+      if (direction) params.direction = direction;
+
+      const response = await axiosInstance.get("/team/joined-teams", { params });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching topics:", error);
+      throw error;
+    }
+  },
+  getJoinRequest: async (page: number, limit: number, search = "", sortElement = "", direction = "",status="PENDING") => {
+    try {
+      const params: any = { page, limit ,status};
+      if (search) params.search = search;
+      if (sortElement) params.sortElement = sortElement;
+      if (direction) params.direction = direction;
+
+      const response = await axiosInstance.get("/team/join-requests", { params });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching topics:", error);
+      throw error;
+    }
+  },
+  sendJoinRequest: async (id) => {
+    try {
+    
+      const response = await axiosInstance.post(`/team/${id}/join`,);
+      console.log(response);
+      
+    } catch (error) {
+      console.error("Error fetching topics:", error);
+      throw error;
+    }
+  },
 }
 
 export default TeamService;
